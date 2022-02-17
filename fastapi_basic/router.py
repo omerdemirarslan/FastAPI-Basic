e35535a9cd863a40141d1e679723c3b8b4acd284
Akinonomer02082021 <omer.demirarslan@akinon.com>
2022-02-17 18:14:16 +0300

    FAST-03 Comment: Create Update Method For Users Model and Create User Register Endpoint

--- fastapi_basic/router.py
+++ fastapi_basic/router.py
@@ -1,6 +1,13 @@
-from fastapi_basic import app
+from fastapi_basic import app, Request
 
 
-@app.get("/")
-def home():
-    return {"Hello": "World"}
+@app.get("/api/v1/users/register", tags=["user register"])
+def user_register(user_post_data: Request):
+    """
+    This method Handle User Register Requests
+    :param user_post_data:
+    :return:
+    """
+    user_converted_data = await user_post_data.json()
+
+    return user_converted_data
